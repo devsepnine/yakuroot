50dad95f270ce566db9d52f29005c39ea28978b6
maerad11 <zpdlwls2@gmail.com>
2019-07-08 18:04:13 +0900

    자유게시판 수정안됨

--- yakuroot/src/f_board/beans/f_board_edit.java
+++ yakuroot/src/f_board/beans/f_board_edit.java
@@ -23,11 +23,11 @@ protected void service(HttpServletRequest req, HttpServletResponse resp) throws 
 		bdto.setF_title(title);
 		bdto.setF_content(content);
 		bdto.setF_no(no);
-		bdao.edit(bdto);	
 		
 		f_boardDto bdto1 = bdao.get(no);		
 
 		req.setAttribute("bdto", bdto1);	
+		bdao.edit(bdto);	
 		RequestDispatcher dispatcher = req.getRequestDispatcher("f_content.do?no="+bdto.getF_no());
 		dispatcher.forward(req, resp);
 		
