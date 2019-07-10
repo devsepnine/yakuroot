9e2e7f621cd5f2630ba493f6857334a6875d60df
imdoyoung <50163340+imdoyoung@users.noreply.github.com>
2019-07-10 14:59:15 +0900

    yongyong

--- yakuroot/src/player/beans/BatterDao.java
+++ yakuroot/src/player/beans/BatterDao.java
@@ -1,5 +1,15 @@
 package player.beans;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class BatterDao {
+	
+//	연결 메소드
+	public Connection getConnection() throws Exception {
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "yakuroot", "baseball");
+		return con;
+	}
 
 }
