932fc8d2907ae4efb0240128ca74a9092b7f9461
kimnayeoni <50163350+kimnayeoni@users.noreply.github.com>
2019-07-12 11:18:05 +0900

    Edit_info, Edit_pw, SamplePw
    edit_info.jsp
    EditInfoServlet.java
    -> 관리자의 회원정보 수정
    
    edit_pw.jsp
    EditPwServlet.java
    -> 관리자의 회원 비밀번호 임시발급
    
    SamplePw
    -> member.util
       임시비밀번호 생성

---
+++ yakuroot/src/admin/EditInfoServlet.java
@@ -0,0 +1,56 @@
+package admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import member.beans.MemberDao;
+import member.beans.MemberDto;
+
+
+
+@WebServlet(urlPatterns = "/admin/edit_info.do")
+public class EditInfoServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher = req.getRequestDispatcher("edit_info.jsp");
+		
+		dispatcher.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		try {
+			
+			MemberDto mdto = new MemberDto();
+			mdto.setM_id(req.getParameter("m_id"));
+			mdto.setM_name(req.getParameter("m_name"));
+			mdto.setM_birth(req.getParameter("m_birth"));
+			mdto.setM_phone(req.getParameter("m_phone"));
+			mdto.setM_email(req.getParameter("m_email"));
+			mdto.setM_postcode(req.getParameter("m_postcode"));
+			mdto.setM_addr1(req.getParameter("m_addr1"));
+			mdto.setM_addr2(req.getParameter("m_addr2"));
+			mdto.setM_fav(req.getParameter("m_fav"));
+			mdto.setM_auth(req.getParameter("m_auth"));
+			
+			MemberDao mdao = new MemberDao();
+			mdao.edit_info(mdto);
+			
+			resp.sendRedirect("info.do?m_id="+mdto.getM_id());		
+
+			
+		} catch (Exception e) {
+			resp.sendError(500);
+			e.printStackTrace();
+		}
+		
+	}
+}
