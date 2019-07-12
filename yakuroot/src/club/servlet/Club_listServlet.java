13026305ac57e9626a9f0284770bd77faa6613d5
imdoyoung <50163340+imdoyoung@users.noreply.github.com>
2019-07-12 17:24:20 +0900

    club_list.jsp, Club_listServlet club_list.jsp 수정 완료 이제 내용 잘 나옴ㅎ Club_listServlet 수정

--- yakuroot/src/club/servlet/Club_listServlet.java
+++ yakuroot/src/club/servlet/Club_listServlet.java
@@ -19,13 +19,11 @@ public class Club_listServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
-			//parameter에 있는 c_no를 받아와서 c_no의 구단정보를 cdto에 넣는다
-			int c_no = Integer.parseInt(req.getParameter("c_no"));
 			ClubDao cdao = new ClubDao();
 			List<ClubDto> cdto = cdao.getClub();
 			
 			//cdto를 parameter에 추가해서 club_info로 넘겨준다
-			req.setAttribute("cdto", cdto);
+			req.setAttribute("cdtolist", cdto);
 			
 			//포워딩
 			RequestDispatcher dispatcher = req.getRequestDispatcher("club_list.jsp");
